b1f62414a9593388ae14c6da17607c3229683d75
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-07-25 16:05:04 +0900

    Next.js チュートリアル APIルートまで

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Date from '../components/date';
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, this is Yasuhiro Fujimoto.<br />
+          I used to work in web production. Now, I am studying web application development.<br />
+          LInkedIn is <a href='www.linkedin.com/in/yasuhiro-fujimoto-7b7288213'>this</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://blog-yasu.com/">My Blog</a>.)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
